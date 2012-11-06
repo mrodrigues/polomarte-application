6658ab1fa6abe913eb753df8d3589849dfc4c636
mrodrigues <mrodrigues.uff@gmail.com>
2012-11-06 19:48:11 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PolomarteApplication::Application.config.secret_token = 'ea4360984fedfdc9ea264c257959cfaeba0671ab6becc6ee5eeb5f4bf7dad2eb7d05a0a8e3aeb4bd0f442d6ec2502c9b59d44e01ab96a501af84e975ef69f9cd'
